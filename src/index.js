be2aae7f617d060be264d54ef1a172a05f20e3db
RafeAaron <rafeaaron21@gmail.com>
2026-02-05 17:18:31 +0300

    Retrieving post data function working

--- src/index.js
+++ src/index.js
@@ -4,12 +4,41 @@ const { URL } = require("url")
 
 const port = 8020
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const fullUrl = new URL(req.url, `http://${req.headers.host}`);
     const searchParams = fullUrl.searchParams;
 
-    res.end("<p>Welcome to the world of vibes</p>")
+    switch(fullUrl.pathname){
+
+        case "/welcome": 
+            res.end("<p>Welcome to the world of vibes</p>")
+            break;
+        
+        case "/authenticate":
+            let data = "";
+            let inter_m;
+            let final = {};
+
+            req.on("data", blob => 
+                data += blob
+            );
+
+            req.on("end", () => {
+
+                inter_m = data.split("&")
+                inter_m.forEach((value) => {
+                    const inter_value = value.split("=")
+                    final[inter_value[0]] = inter_value[1]
+                })
+                res.end(`User data: ${JSON.stringify(final)}`)
+            })
+            
+            break;
+
+        default:
+            res.end("<div style='width: 100vw;height: 100vh;display: flex;align-items: center;justify-content: center'><p>404 page not found</p></div>")
+    }
 
 }).listen(port, (err) => {
     if(err){
@@ -20,3 +49,26 @@ const server = http.createServer((req, res) => {
     console.log(`The server has started successfully at port ${port}`)
 
 })
+
+async function getDataFromPostRequest(request){
+    let data = "";
+    let inter_m;
+    let final = {};
+
+    request.on("data", blob => 
+        data += blob
+    );
+
+    request.on("end", () => {
+        console.log(data);
+
+        inter_m = data.split("&")
+        inter_m.forEach((value) => {
+            const inter_value = value.split("=")
+            final[inter_value[0]] = inter_value[1]
+            console.log(inter_m)
+        })
+
+        return final
+    })
+}
